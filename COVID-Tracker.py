9e1af04b74614d37c64396b2c8323d6cc67ce96a
Harish Lal <987456+Lal-Jr@users.noreply.github.com>
2020-10-10 09:49:04 +0530

    Update COVID-Tracker.py

--- COVID-Tracker.py
+++ COVID-Tracker.py
@@ -5,5 +5,11 @@ covid = Covid()
 data = covid.get_status_by_country_name(country)
 cadr = {
     key: data[key]
-    for key in data.keys
-}
+    for key in data.keys() & {"confirmed","active","deaths","recovered"}
+    }
+n = list(cadr.keys())
+v = list(cadr.values())
+print(cadr)
+pyplot.title(country)
+pyplot.bar(range(len(cadr)), v, tick_label=n) 
+pyplot.show()
